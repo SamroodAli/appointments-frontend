d20859b33255f75ab289c535420bb1d70757dd8c
samroodAli <devsamrood@gmail.com>
2021-11-25 15:30:51 +0530

    Fix: Remove style.css import from index js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
+import redux from './redux';
 import App from './components/App';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={redux.store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
