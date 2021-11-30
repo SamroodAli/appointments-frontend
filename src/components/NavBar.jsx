ed64e718b40383de1b35c0ffceece8a09de9ecf2
samroodAli <devsamrood@gmail.com>
2021-11-30 20:52:39 +0530

    Auth:Signout link if the user is already signed in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,24 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const NavBar = () => (
-  <div>
-    <Link to="/signup">Sign up</Link>
-    <Link to="/signin">Sign in</Link>
-  </div>
-);
+const NavBar = () => {
+  const { currentUser: { username } } = useSelector((state) => state);
+
+  return (
+    <div>
+      {
+        username && <Link to="/signout">Sign out</Link>
+      }
+      {
+        !username && (
+          <>
+            <Link to="/signup">Sign up</Link>
+            <Link to="/signin">Sign in</Link>
+          </>
+        )
+      }
+    </div>
+  );
+};
 
 export default NavBar;
