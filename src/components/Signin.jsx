ae5568b416ffdfb338e87efdb010a25fe3781e12
samroodAli <devsamrood@gmail.com>
2021-11-30 20:12:48 +0530

    Auth: Programmatically Navigate user on successful signin

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import useActions from '../hooks/useActions';
 
 const Signin = () => {
   const [email, setEmail] = useState('samrood@gmailexample22.com');
   const [password, setPassword] = useState('password');
   const { signin } = useActions();
+  const { currentUser } = useSelector();
+  const navigate = useNavigate();
+
+  if (currentUser.username) {
+    navigate('/');
+  }
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -12,17 +20,20 @@ const Signin = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="username">
-        Enter email
-        <input value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <label htmlFor="username">
-        Enter password
-        <input value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <div>
+      {currentUser.errorMessage && <p>{currentUser.errorMessage}</p>}
+      <form onSubmit={onSubmit}>
+        <label htmlFor="username">
+          Enter email
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label htmlFor="username">
+          Enter password
+          <input value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   );
 };
 
