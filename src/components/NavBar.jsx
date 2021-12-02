b566595e675dae3b91a5ab1d2e11d684f74d4160
samroodAli <devsamrood@gmail.com>
2021-12-03 02:26:44 +0530

    Navbar:Style navbar to match design spec

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,10 @@
+import { createPortal } from 'react-dom';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { QueryCache, MutationCache } from 'react-query';
 import useActions from '../hooks/useActions';
+
+const navBarContainer = document.getElementById('navLinks');
 
 const NavBar = () => {
   const { currentUser: { username } } = useSelector((state) => state);
@@ -15,26 +18,24 @@ const NavBar = () => {
     navigate('/');
   };
 
-  return (
-    <div>
-      {
-        username && (
-          <>
-            <button type="button" onClick={onSignOut}>Sign out</button>
-            <Link to="/appointments">My Appointments</Link>
-          </>
-        )
-      }
-      {
-        !username && (
-          <>
-            <Link to="/signup">Sign up</Link>
-            <Link to="/signin">Sign in</Link>
-          </>
-        )
-      }
-      <Link to="/teachers">Teachers</Link>
-    </div>
+  const links = [
+    { to: '/teachers', text: 'Teachers', locked: false },
+    { to: '/appointments', text: 'Appointments', locked: !username },
+    { to: '/signin', text: 'Sign In', locked: !!username },
+    { to: '/signup', text: 'Sign Up', locked: !!username },
+    {
+      to: '/signout', text: 'Sign Out', locked: !username, onClick: onSignOut,
+    },
+
+  ].filter(({ locked }) => !locked).map((link) => (
+    <li key={link.to}>
+      <Link to={link.to} className={window.location.pathname === link.to ? 'active' : ''}>{link.text}</Link>
+    </li>
+  ));
+
+  return createPortal(
+    links,
+    navBarContainer,
   );
 };
 
