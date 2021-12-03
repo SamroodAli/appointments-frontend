bd14f829c06e5cc1598a04494747eb3bc2594302
samroodAli <devsamrood@gmail.com>
2021-12-03 21:06:39 +0530

    Appointments:Programmatically render empty appointments page headings

--- src/components/Appointments.jsx
+++ src/components/Appointments.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import getAppointments from '../api/getAppointments';
 import useLockedData from '../hooks/useLockedData';
 import AppointmentsList from './AppointmentList';
@@ -14,12 +15,26 @@ const Appointments = () => {
   return (
     <div>
       <h1>Appointments</h1>
-      <h2>Today</h2>
-      <AppointmentsList collection={data.today} />
-      <h2>Upcoming</h2>
-      <AppointmentsList collection={data.upcoming} />
-      <h2>Past</h2>
-      <AppointmentsList collection={data.past} />
+      {
+        Object.entries(data).map(([key, value]) => {
+          if (!value.length) {
+            return (
+              <h2>
+                {`No ${key === 'today' ? '' : key} appointments ${key !== 'today' ? '' : key}`}
+              </h2>
+            );
+          }
+          return (
+            <React.Fragment key={key}>
+              <h2>
+                {`${key}${key === 'today' ? "'s" : ''} appointments`}
+              </h2>
+              <AppointmentsList collection={value} />
+            </React.Fragment>
+          );
+        })
+      }
+
     </div>
   );
 };
