061e05811f130af6a08cb6566759fd1a6726c9f6
samroodAli <devsamrood@gmail.com>
2021-12-04 18:28:08 +0530

    Style: Style teacher card for md screens

--- src/components/horizontal-scroll/card.jsx
+++ src/components/horizontal-scroll/card.jsx
@@ -24,16 +24,16 @@ export default function Card({
     <button
       type="button"
       onClick={onClick}
-      className="card rounded mb-16 mx-12 h-56 w-8/12 rounded-3xl mt-20"
+      className="card rounded mb-16 lg:mx-12 h-44 lg:h-56 w-9/12 lg:w-8/12 rounded-3xl mt-20"
       // shuffle background color
       style={{ backgroundColor: COLOR_ENUM[item.index % COLOR_ENUM.length] }}
       tabIndex={0}
     >
-      <div className="w-52 m-10 bg-red-100 relative bottom-20 left-20 rounded-full">
-        <img className="non-draggable h-52 rounded-full relative right-24 bottom-8 mx-auto filter drop-shadow-2xl" src="https://1.bp.blogspot.com/-vhmWFWO2r8U/YLjr2A57toI/AAAAAAAACO4/0GBonlEZPmAiQW4uvkCTm5LvlJVd_-l_wCNcBGAsYHQ/s16000/team-1-2.jpg" alt="profile_picture" />
+      <div className=" w-36 lg:w-52 m-16 lg:m-10 bg-red-100 relative bottom-20 left-8 lg:left-20 rounded-full">
+        <img className="non-draggable h-36 lg:h-52 rounded-full relative right-16 lg:right-24 bottom-6 lg:bottom-8 mx-auto filter drop-shadow-2xl" src="https://1.bp.blogspot.com/-vhmWFWO2r8U/YLjr2A57toI/AAAAAAAACO4/0GBonlEZPmAiQW4uvkCTm5LvlJVd_-l_wCNcBGAsYHQ/s16000/team-1-2.jpg" alt="profile_picture" />
       </div>
-      <h5 className="text-center mt-3 relative bottom-52 left-36 font-mono">{item.name}</h5>
-      <p className="text-center relative bottom-36 white">Software engineer at Linkedin</p>
+      <h5 className="text-center mt-3 relative bottom-48 lg:bottom-52 left-20 lg:left-36 font-mono">{item.name}</h5>
+      <p className="text-center relative bottom-44 lg:bottom-36 white">Software engineer at Linkedin</p>
       <hr className="teacher-line" />
     </button>
   );
