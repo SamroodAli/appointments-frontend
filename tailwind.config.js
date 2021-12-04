34ecc134d7ad2603c7f418ab0843e7bf37b48aaf
samroodAli <devsamrood@gmail.com>
2021-12-04 19:39:58 +0530

    Style: Style the signin page to resemble design spec sheet with green color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#97BF10',
+        secondary: '#FFCF00',
+      },
+    },
   },
   variants: {
     extend: {},
