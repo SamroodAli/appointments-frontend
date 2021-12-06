2118e34df8c5f7213120534aa75cbc9c792fdd86
samroodAli <devsamrood@gmail.com>
2021-12-06 19:26:35 +0530

    Link:Add link to teachers to home page hero section buttons and animated down arrow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,22 +6,27 @@ const Home = () => (
       <h1 className="text-6xl lg:text-8xl font-semibold text-gray-900 leading-none">Codezilla appointments </h1>
       <div className="text-6xl text-gray-900 leading-none">Learn from the best</div>
       <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">All from the comfort of your home.</div>
-      <button
-        type="button"
-        className="mt-6 px-8 py-4 rounded-full font-normal
-      tracking-wide bg-gradient-to-b from-green-600 to-green-700
+      <Link to="/teachers">
+        <button
+          type="button"
+          className="mt-6 px-8 py-4 rounded-full font-normal
+        tracking-wide bg-gradient-to-b from-green-600 to-green-700
         text-white outline-none focus:outline-none hover:shadow-lg
         hover:from-green-700 transition duration-200 ease-in-out"
-      >
-        Check out our masters
-      </button>
+        >
+          Check out our masters
+        </button>
+      </Link>
     </div>
-    <div className="mt-12 lg:mt-32 lg:ml-20 text-left">
-      <bottom type="button" className="flex items-center justify-center w-12 h-12 rounded-full bg-cool-gray-100 text-gray-800 animate-bounce hover:text-gray-900 hover:bg-cool-gray-50 transition duration-300 ease-in-out cursor-pointer">
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-        </svg>
-      </bottom>
+    <div className="mt-12 lg:mt-12 lg:ml-20 text-left">
+      <Link to="/teachers">
+        <bottom type="button" className="flex items-center justify-center w-12 h-12 rounded-full bg-cool-gray-100 text-gray-800 animate-bounce hover:text-gray-900 hover:bg-cool-gray-50 transition duration-300 ease-in-out cursor-pointer">
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+          </svg>
+        </bottom>
+
+      </Link>
     </div>
   </div>
 );
