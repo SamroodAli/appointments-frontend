cc527a4aa88ad7ccf26ff97ef3f298e56810d06d
samroodAli <devsamrood@gmail.com>
2021-12-23 13:47:17 +0530

    Redux-thunk:Make redux thunk functions more readable with named functions

--- build.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-
-execSync('npm run build');
-console.log('Finished building');
--- src/redux/action-creators/index.js
+++ src/redux/action-creators/index.js
@@ -1,64 +1,73 @@
 import rails from '../../api/rails';
 import {
-  onCurrentUser, onCurrentUserError, onRedirect, onSignout,
+  onCurrentUser,
+  onCurrentUserError,
+  onRedirect,
+  onSignout,
 } from '../actions';
 
 export const autoSignin = (username) => onCurrentUser(username);
 export const signinError = (message) => onCurrentUserError(message);
 
-export const signup = (
+export function signup(
   username,
   email,
   password,
   passwordConfirmation,
   navigate,
-) => async (dispatch, getState) => {
-  const { redirect } = getState();
+) {
+  return async function reduxThunkSignUp(dispatch, getState) {
+    const { redirect } = getState();
 
-  try {
-    const { data } = await rails.post('/auth/signup', {
-      user: {
-        username,
-        email,
-        password,
-        password_confirmation: passwordConfirmation,
-      },
-    });
-    dispatch(onCurrentUser(data.username));
-    navigate(redirect);
-  } catch (err) {
-    dispatch(onCurrentUserError(err.response.data));
-  }
-};
-
-export const signin = (email, password, navigate) => async (dispatch, getState) => {
-  const { redirect } = getState();
-
-  try {
-    const { data } = await rails.post('/auth/signin', {
-      auth: {
-        email,
-        password,
-      },
-    });
-    if (data) {
+    try {
+      const { data } = await rails.post('/auth/signup', {
+        user: {
+          username,
+          email,
+          password,
+          password_confirmation: passwordConfirmation,
+        },
+      });
       dispatch(onCurrentUser(data.username));
-    }
-    if (navigate) {
       navigate(redirect);
+    } catch (err) {
+      dispatch(onCurrentUserError(err.response.data));
     }
-  } catch (err) {
-    dispatch(onCurrentUserError('Invalid credentials'));
-  }
-};
+  };
+}
 
-export const signout = () => async (dispatch) => {
-  try {
-    await rails.delete('/auth/signout');
-    dispatch(onSignout());
-  } catch (err) {
-    console.error(err);
-  }
-};
+export function signin(email, password, navigate) {
+  return async function reduxThunkSignin(dispatch, getState) {
+    const { redirect } = getState();
+
+    try {
+      const { data } = await rails.post('/auth/signin', {
+        auth: {
+          email,
+          password,
+        },
+      });
+      if (data) {
+        dispatch(onCurrentUser(data.username));
+      }
+      if (navigate) {
+        navigate(redirect);
+      }
+    } catch (err) {
+      dispatch(onCurrentUserError('Invalid credentials'));
+    }
+  };
+}
+
+export function signout() {
+  return async function reduxThunkSignout(dispatch) {
+    try {
+      await rails.delete('/auth/signout');
+      dispatch(onSignout());
+    } catch (err) {
+      dispatch(onCurrentUser(null));
+    }
+  };
+}
 
 export const storeRedirect = (route) => onRedirect(route);
