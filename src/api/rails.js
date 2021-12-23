59f781de337564259d3ae86d1f08a4a97216e0ca
samroodAli <devsamrood@gmail.com>
2021-12-23 13:07:12 +0530

    Env:Add environment variable for rails backend url

--- src/api/rails.js
+++ src/api/rails.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const rails = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.API_URL,
 });
 
 export default rails;
